9c7e4ad0ee592a07ea232d93553ab3d0c9d5c2eb
Stephen OBrien <wayofthepie@gmail.com>
2021-02-12 21:48:58 +0000

    Refactor main, add write_color

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,36 @@
-use std::{error::Error, fs::OpenOptions, io::Write, ops};
+use std::{error::Error, io::Write};
+use vec3::Vec3;
 mod vec3;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let image_width: usize = 256;
     let image_height: usize = 256;
-    let mut file = OpenOptions::new()
-        .create(true)
-        .write(true)
-        .open("riaw.ppm")?;
-    let mut buf = String::new();
-    buf.push_str(&format!("P3\n{} {}\n255\n", image_width, image_height));
-
+    let mut stdout = std::io::stdout();
     for j in (0..image_height).rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..image_width {
-            let r = i as f32 / (image_width - 1) as f32;
-            let g = j as f32 / (image_height - 1) as f32;
-            let b = 0.25;
-            let val = 255.99;
-            let ir = (val * r) as usize;
-            let ig = (val * g) as usize;
-            let ib = (val * b) as usize;
-            buf.push_str(&format!("{} {} {}\n", ir, ig, ib));
+            let color = Vec3 {
+                x: (i as f32 / (image_width - 1) as f32),
+                y: (j as f32 / (image_height - 1) as f32),
+                z: 0.25,
+            };
+            write_color(&stdout, color)?;
         }
     }
-    file.write_all(buf.as_bytes())?;
-    file.flush()?;
+    stdout.flush()?;
+    Ok(())
+}
+
+fn write_color(mut stdout: &std::io::Stdout, color: Vec3) -> Result<(), Box<dyn Error>> {
+    let c = 255.99;
+    stdout.write_all(
+        &format!(
+            "{} {} {}\n",
+            (c * color.x) as usize,
+            (c * color.y) as usize,
+            (c * color.z) as usize
+        )
+        .as_bytes(),
+    )?;
     Ok(())
 }
--- src/vec3.rs
+++ src/vec3.rs
@@ -2,9 +2,9 @@ use std::ops;
 
 #[derive(Debug, PartialEq)]
 pub struct Vec3 {
-    x: f32,
-    y: f32,
-    z: f32,
+    pub x: f32,
+    pub y: f32,
+    pub z: f32,
 }
 
 impl Vec3 {
