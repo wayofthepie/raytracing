5525c97e7675e63345dc3049c82c8a192acfbc74
Stephen OBrien <wayofthepie@gmail.com>
2021-02-12 20:37:57 +0000

    Add div/add assign impls

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{error::Error, fs::OpenOptions, io::Write};
+use std::{error::Error, fs::OpenOptions, io::Write, ops};
 
 fn main() -> Result<(), Box<dyn Error>> {
     let image_width: usize = 256;
@@ -13,8 +13,8 @@ fn main() -> Result<(), Box<dyn Error>> {
     for j in (0..image_height).rev() {
         eprintln!("Scanlines remaining: {}", j);
         for i in 0..image_width {
-            let r = i as f64 / (image_width - 1) as f64;
-            let g = j as f64 / (image_height - 1) as f64;
+            let r = i as f32 / (image_width - 1) as f32;
+            let g = j as f32 / (image_height - 1) as f32;
             let b = 0.25;
             let val = 255.99;
             let ir = (val * r) as usize;
@@ -30,18 +30,18 @@ fn main() -> Result<(), Box<dyn Error>> {
 
 #[derive(Debug, PartialEq)]
 struct Vec3 {
-    x: f64,
-    y: f64,
-    z: f64,
+    x: f32,
+    y: f32,
+    z: f32,
 }
 
 impl Vec3 {
-    pub fn new(x: f64, y: f64, z: f64) -> Self {
+    pub fn new(x: f32, y: f32, z: f32) -> Self {
         Self { x, y, z }
     }
 }
 
-impl std::ops::Neg for Vec3 {
+impl ops::Neg for Vec3 {
     type Output = Vec3;
 
     fn neg(self) -> Self::Output {
@@ -51,6 +51,30 @@ impl std::ops::Neg for Vec3 {
             y: -y,
             z: -z,
         }
+    }
+}
+
+impl ops::AddAssign<f32> for Vec3 {
+    fn add_assign(&mut self, rhs: f32) {
+        self.x = self.x + rhs;
+        self.y = self.y + rhs;
+        self.z = self.z + rhs;
+    }
+}
+
+impl ops::MulAssign<f32> for Vec3 {
+    fn mul_assign(&mut self, rhs: f32) {
+        self.x = self.x * rhs;
+        self.y = self.y * rhs;
+        self.z = self.z * rhs;
+    }
+}
+
+impl ops::DivAssign<f32> for Vec3 {
+    fn div_assign(&mut self, rhs: f32) {
+        self.x = self.x / rhs;
+        self.y = self.y / rhs;
+        self.z = self.z / rhs;
     }
 }
 
@@ -70,6 +94,57 @@ mod test {
                 x: -x,
                 y: -y,
                 z: -z
+            }
+        );
+    }
+
+    #[test]
+    fn mul_assign_should_perform_correctly() {
+        let x = 1.0;
+        let y = 2.0;
+        let z = 3.0;
+        let mut vec = Vec3 { x, y, z };
+        vec *= 2.0;
+        assert_eq!(
+            vec,
+            Vec3 {
+                x: 2.0,
+                y: 4.0,
+                z: 6.0
+            }
+        );
+    }
+
+    #[test]
+    fn add_assign_should_perform_correctly() {
+        let x = 1.0;
+        let y = 2.0;
+        let z = 3.0;
+        let mut vec = Vec3 { x, y, z };
+        vec += 1.0;
+        assert_eq!(
+            vec,
+            Vec3 {
+                x: x + 1.0,
+                y: y + 1.0,
+                z: z + 1.0
+            }
+        );
+    }
+
+    #[test]
+    fn div_assign_should_perform_correctly() {
+        let x = 2.0;
+        let y = 3.0;
+        let z = 5.0;
+        let mut vec = Vec3 { x, y, z };
+        vec /= 2.0;
+        assert_eq!(
+            vec,
+            Vec3 {
+                x: x / 2.0,
+                y: y / 2.0,
+                z: z / 2.0
             }
         );
     }
