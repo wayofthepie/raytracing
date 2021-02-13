761a0b7dcadfa3ccb010a03d70e4fc09a7c4c3fe
Stephen OBrien <wayofthepie@gmail.com>
2021-02-13 15:09:53 +0000

    Add up to 6.11, surface normals

--- src/ray.rs
+++ src/ray.rs
@@ -16,19 +16,25 @@ impl Ray {
 }
 
 pub fn ray_color(ray: &Ray) -> Vec3 {
-    if hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
-        return Vec3::new(1.0, 0.0, 0.0);
+    let t = hit_sphere(Vec3::new(0.0, 0.0, -1.0), 0.5, ray);
+    if t > 0.0 {
+        let n = unit_vector(ray.at(t) - Vec3::new(0.0, 0.0, -1.0));
+        return Vec3::new(n.x + 1.0, n.y + 1.0, n.z + 1.0) * 0.5;
     }
     let unit_direction = unit_vector(ray.direction);
     let t = 0.5 * (unit_direction.y + 1.0);
     Vec3::new(1.0, 1.0, 1.0) * (1.0 - t) + Vec3::new(0.5, 0.7, 1.0) * t
 }
 
-pub fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> bool {
+pub fn hit_sphere(center: Vec3, radius: f32, ray: &Ray) -> f32 {
     let oc = ray.origin - center;
     let a = dot(ray.direction, ray.direction);
     let b = 2.0 * dot(oc, ray.direction);
     let c = dot(oc, oc) - radius * radius;
     let discriminant = b * b - 4.0 * a * c;
-    discriminant > 0.0
+    if discriminant < 0.0 {
+        -1.0
+    } else {
+        (-b - discriminant.sqrt()) / (2.0 * a)
+    }
 }
